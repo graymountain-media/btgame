e1413be853a73e7df5320fe70acc0c8fa7534d72
Brock Boyington <brock@Brocks-MacBook-Pro.local>
2018-05-15 11:18:20 -0600

    PlayerController

--- btgame/btgame/ModelControllers/PlayerController.swift
+++ btgame/btgame/ModelControllers/PlayerController.swift
@@ -7,3 +7,18 @@
 //
 
 import Foundation
+import MultipeerConnectivity
+
+class PlayerController {
+    
+    static func create(displayName: String, id: MCPeerID, isAdvertiser: Bool, timelineID: UUID) -> Player {
+        let player = Player(displayName: displayName, id: id, isAdvertiser: isAdvertiser, timelineID: timelineID)
+        return player
+    }
+    
+    static func delete(player: Player, game: Game) {
+        if let playerIndex = game.players.index(of: player) {
+            game.players.remove(at: playerIndex)
+        }
+    }
+}
