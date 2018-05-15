0adcf36090cdd77836f3530068ab6abb86d1ab7c
Jake Gray <grayjake14@gmail.com>
2018-05-15 08:59:09 -0600

    Add all necessary files for base project

---
+++ btgame/btgame/ModelControllers/RoundController.swift
@@ -0,0 +1,9 @@
+//
+//  RoundController.swift
+//  btgame
+//
+//  Created by Jake Gray on 5/15/18.
+//  Copyright © 2018 Jake Gray. All rights reserved.
+//
+
+import Foundation
---
+++ btgame/btgame/Models/Player.swift
@@ -0,0 +1,9 @@
+//
+//  Player.swift
+//  btgame
+//
+//  Created by Jake Gray on 5/15/18.
+//  Copyright © 2018 Jake Gray. All rights reserved.
+//
+
+import Foundation
