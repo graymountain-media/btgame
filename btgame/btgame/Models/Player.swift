0a61c0d764d0bbf103fb00140decdd23bfad1fc2
Brock Boyington <brock@Brocks-MacBook-Pro.local>
2018-05-15 09:48:01 -0600

    Created player model

--- btgame/btgame/Models/Player.swift
+++ btgame/btgame/Models/Player.swift
@@ -7,3 +7,18 @@
 //
 
 import Foundation
+import MultipeerConnectivity
+
+struct Player {
+    let displayName: String
+    let id: MCPeerID
+    let isAdvertiser: Bool
+    let timelineID: UUID
+    
+    init(displayName: String, id: MCPeerID, isAdvertiser: Bool, timelineID: UUID) {
+        self.displayName = displayName
+        self.id = id
+        self.isAdvertiser = isAdvertiser
+        self.timelineID = timelineID
+    }
+}
