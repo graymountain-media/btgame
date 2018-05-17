d43c9b83bb93d42c9a47b62d1afdafe2bf54b5bb
Brock Boyington <brock@Brocks-MacBook-Pro.local>
2018-05-17 10:09:26 -0600

    Created InstructionsVC, CanvasView, CanvasController files

---
+++ btgame/btgame/ViewControllers/InstructionsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  InstructionsViewController.swift
+//  btgame
+//
+//  Created by Brock Boyington on 5/17/18.
+//  Copyright © 2018 Jake Gray. All rights reserved.
+//
+
+import UIKit
+
+class InstructionsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
