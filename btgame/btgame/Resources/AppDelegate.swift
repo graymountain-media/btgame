2a114ac066a3fceaf6ed4d836793f15471df23ad
Jake Gray <grayjake14@gmail.com>
2018-05-18 11:32:12 -0600

    finish timline passing

--- btgame/btgame/Resources/AppDelegate.swift
+++ btgame/btgame/Resources/AppDelegate.swift
@@ -20,7 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
-        window?.rootViewController = UINavigationController(rootViewController: CanvasViewController())
+        window?.rootViewController = UINavigationController(rootViewController: WelcomeViewController())
         
         // Override point for customization after application launch.
         return true
