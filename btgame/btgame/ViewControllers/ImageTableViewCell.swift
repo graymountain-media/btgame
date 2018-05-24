62234fac09c13f55ded9cc38ae9e421a6ee9e9fb
Jake Gray <grayjake14@gmail.com>
2018-05-24 11:52:56 -0600

    facelift to results view

--- btgame/btgame/ViewControllers/ImageTableViewCell.swift
+++ btgame/btgame/ViewControllers/ImageTableViewCell.swift
@@ -14,39 +14,41 @@ class ImageTableViewCell: UITableViewCell {
     
     var playerNameLabel: UILabel = {
         let label = UILabel()
-        label.backgroundColor = UIColor.white
         label.layer.masksToBounds = true
         label.font = UIFont.boldSystemFont(ofSize: 15)
         label.textColor = UIColor.mainScheme1()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.textAlignment = .center
-        label.layer.borderColor = UIColor.mainScheme1().cgColor
-        label.layer.borderWidth = 1.0
-        label.backgroundColor = UIColor.white
+        label.textAlignment = .left
         return label
     }()
     
     var sketchImageView: UIImageView = {
         
         let imageView = UIImageView()
-        imageView.backgroundColor = UIColor.mainOffWhite()
+        imageView.backgroundColor = UIColor.white
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFit
+        imageView.layer.cornerRadius = 10
+        imageView.layer.borderWidth = 2
+        imageView.layer.borderColor = UIColor.mainScheme2().cgColor
+        imageView.layer.shadowRadius = 10
+        imageView.layer.shadowColor = UIColor.mainHighlight().cgColor
+        imageView.layer.shadowOpacity = 0.3
         return imageView
     }()
     
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        self.backgroundColor = UIColor.white
+        
         contentView.addSubview(sketchImageView)
         contentView.addSubview(playerNameLabel)
-        sketchImageView.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
-        sketchImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor).isActive = true
-        sketchImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
-        sketchImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+        sketchImageView.topAnchor.constraint(equalTo: playerNameLabel.bottomAnchor, constant: 8).isActive = true
+        sketchImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 32).isActive = true
+        sketchImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -32).isActive = true
+        sketchImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -19).isActive = true
         
-        playerNameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
-        playerNameLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+        playerNameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 5).isActive = true
+        playerNameLabel.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
         playerNameLabel.widthAnchor.constraint(equalToConstant: 100.0) .isActive = true
         playerNameLabel.heightAnchor.constraint(equalToConstant: 30.0) .isActive = true
 
@@ -56,7 +58,7 @@ class ImageTableViewCell: UITableViewCell {
         if let round = round {
             if round.isImage {
                 if let image = round.imageData {
-                    self.backgroundColor = UIColor.mainScheme3()
+                    self.backgroundColor = UIColor.mainOffWhite()
                     sketchImageView.image = UIImage(data: image)
                     playerNameLabel.text = round.owner.displayName
                 }
