e7294f3eebc1d09352b6d3ebb908c182163705e4
Jake Gray <grayjake14@gmail.com>
2018-05-31 09:33:37 -0600

    Updated build version and settings

--- dubNDoodle/dubNDoodle/ViewControllers/ResultsViewController.swift
+++ dubNDoodle/dubNDoodle/ViewControllers/ResultsViewController.swift
@@ -39,12 +39,14 @@ class ResultsViewController: UIPageViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
-
+    // MARK: - Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.delegate = self
         self.dataSource = self
         MCController.shared.delegate = self
+        self.title = "Results"
         
         self.view.backgroundColor = UIColor.mainScheme1()
         navigationController?.navigationBar.isHidden = false
